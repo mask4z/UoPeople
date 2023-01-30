bf3afda67891ecfa1fb7f8c10df2bbd0acc595f2
Renaldo Swanepoel <renaldo.swanepoel16@gmail.com>
2023-01-30 20:03:30 +0200

    Start of first project

---
+++ CS1102/src/main/java/ren/cs1102/CS1102.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ren.cs1102;
+
+/**
+ *
+ * @author renos
+ */
+public class CS1102 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
