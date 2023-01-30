fc755e5539a9811706b8ae9fb75cc8051cdfda06
Renaldo Swanepoel <renaldo.swanepoel16@gmail.com>
2023-01-30 21:31:03 +0200

    Demo of semantic error

--- CS1102/src/main/java/ren/cs1102/CS1102.java
+++ CS1102/src/main/java/ren/cs1102/CS1102.java
@@ -9,6 +9,8 @@ package ren.cs1102;
  * @author renos
  */
 public class CS1102 {
+    
+    int a = "semantic";
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
