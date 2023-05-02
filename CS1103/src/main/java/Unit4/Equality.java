42e04ee366759ac43a21e38d84938ee3d21a1372
Renaldo Swanepoel <renaldo.swanepoel16@gmail.com>
2023-05-02 20:12:29 +0200

    Example of equals overriding

--- CS1103/src/main/java/Unit4/Equality.java
+++ CS1103/src/main/java/Unit4/Equality.java
@@ -5,44 +5,56 @@ import java.util.Random;
 /**
  * This class just demonstrates how an equals() method would be practically
  * used.
- * 
- * @author renos
  *
+ * @author renos
  */
 public class Equality {
 
-	static class Student{
-		private Integer studentId;
-		
-		public Student(int i) {
-			this.studentId = i;
-		}
-		
-		public Student ( ) {
-			
-			Random random = new Random();
-			
-			this.studentId = random.nextInt();
-		}
-	
-		/**
-		 * The equals method is inherited from the class Object.
-		 * 
-		 */
-		private boolean equals(Student st) {
+    static class Student {
+        private Integer studentId;
 
-			if (this.studentId == st.studentId) {
-				return true;
-			} else {
-				return false;
-			}
-	}
-		
-	public static void main(String[] args) {
-		
-		Student st1 = new Student();
-		Student st2 = new Student(003365);
-		
-		
-	}
+        public Student(int i) {
+            this.studentId = i;
+        }
+
+        public Student() {
+
+            Random random = new Random();
+
+            this.studentId = random.nextInt();
+        }
+
+        /**
+         * The equals method is inherited from the class Object.
+         */
+        private boolean equals(Student st) {
+
+            if (this.studentId == st.studentId) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Student st1 = new Student();
+        Student st2 = new Student(003365);
+
+        System.out.println("This equality check will return false as the student's ids aren't the same.");
+        System.out.println("Student 1's id is " + st1.studentId);
+        System.out.println("Student 2's id is " + st2.studentId);
+        System.out.println(st1.equals(st2));
+
+        st1.studentId = st2.studentId;
+
+        System.out.println("This equality check will return true as the student's ids are the same.");
+        System.out.println("Student 1's id is " + st1.studentId);
+        System.out.println("Student 2's id is " + st2.studentId);
+        System.out.println(st1.equals(st2));
+
+
+    }
 }
