569b89f99e97b7c682db6cfd06538d15a67859f9
Renaldo Swanepoel <renaldo.swanepoel16@gmail.com>
2023-05-04 21:37:05 +0200

    Complete Exercise

--- CS1103/src/main/java/Unit4/LearningJournal/SetCalculator.java
+++ CS1103/src/main/java/Unit4/LearningJournal/SetCalculator.java
@@ -1,149 +1,153 @@
 package Unit4.LearningJournal;
 
 import Test.TextIO;
+
 import java.util.TreeSet;
 
 
 /**
- * This program is a very simple "set calculator" that can compute
- * the intersection, union, and set difference of two sets of
- * non-negative integers.  Each line of the user's input contains two
- * such sets, separated by one of the operators +, *, or -, standing
- * for union, intersection, and set difference respectively.  A set
- * must be given in the form of a list of non-negative integers, separated
- * by commas, and enclosed in square brackets.  For example:
- * [1, 2, 3] + [4, 3, 10, 0].  Spaces can occur anywhere in the input.
- * If an error is found in the input, the program will report it.
+ * This program is a set calculator that can compute
+ * the union, intersection, and difference of two sets of
+ * non-negative integers.
+ * For example:
+ * [1, 2, 3] + [4, 0, 2, 3].
+ * Spaces are allowed.
+ * The program will log an error if it occurs.
  * The program ends when the user inputs an empty line.
- *
- * This program is mainly a demonstration of Sets.
  */
 public class SetCalculator {
 
     public static void main(String[] args) {
 
-      System.out.println("This program will compute union, intersection,");
-      System.out.println("and set difference of sets of integers.");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("Enter set computations (press return to end):");
-
-      while (true) {
-         char ch;
-         System.out.print("\n? ");
-         TextIO.skipBlanks();
-         if (TextIO.peek() == '\n') {
-               // The input line is empty. 
-               // Exit the loop and end the program.
-            break;
-         }
-         try {
-            calc(); // Reads and processes one line of input.
-         }
-         catch (IllegalArgumentException e) {
-               // An error was found in the input line.
-            System.out.println("Error in input: " + e.getMessage());
-            System.out.print("Discarding input: ");
-         }
-         do {  // Copy extra characters on line to output.  If there
-               // was no error, then the only character that is copied
-               // is the end-of-line character.
-            ch = TextIO.getAnyChar();
-            System.out.print(ch);
-         } while (ch != '\n');
-      }
-
-   } // end main()
+        System.out.println("This program will compute union, intersection, and difference of sets of integers.");
+        System.out.println();
+        System.out.println("Hit return if you wish to end the program.");
+        System.out.println("Please enter two sets of integers with an operator:\n?");
 
 
-   /**
-    * Read a line of input, consisting of two sets separated by
-    * an operator.  Perform the operation and output the value.
-    * If any syntax error is found in the input, an
-    * IllegalArgumentException is thrown
-    */
-   private static void calc() {
+        while (true) {
 
-      TreeSet<Integer> A, B;  // The two sets of integers.
-      char op;                // The operator, +, *, or -.
+            char mainOperator;
+            TextIO.skipBlanks();
 
-      A = readSet(); // Read the first set.
+            if (TextIO.peek() == '\n') {
 
-      TextIO.skipBlanks();
-      if (TextIO.peek() != '*' && TextIO.peek() != '+'
-                                         && TextIO.peek() != '-')
-         throw new IllegalArgumentException(
-         "Expected *, +, or  - after first set.");
-      op = TextIO.getAnyChar(); // Read the operator.
+                // The input line is empty.
+                break;
+            }
 
-      B = readSet(); // Read the second set.
+            try {
 
-      TextIO.skipBlanks();
-      if (TextIO.peek() != '\n')
-         throw new IllegalArgumentException("Extra unexpected input.");
+                calculateSets(); // Reads and processes one line of input.
 
-      // Perform the operation.  This modifies set A to represent
-      // the answer.  Display the answer by printing out A. The
-      // output format for a set of integers is the same as the
-      // input format used by this program.
+            } catch (IllegalArgumentException iae) {
 
-      if (op == '+')
-         A.addAll(B);     // Union.
-      else if (op == '*')
-         A.retainAll(B);  // Intersection.
-      else
-         A.removeAll(B);  // Set difference.
-     
-      System.out.print("Value:  " + A);
+                System.out.println("Error: " + iae.getMessage()); //prints error message
+            }
 
-   } // end calc()
+            do {
+                mainOperator = TextIO.getAnyChar();
+
+            } while (mainOperator != '\n');
+        }
+
+    } // end of main() method.
 
 
-   /**
-    * Reads a set of non-negative integers from standard input,
-    * and stores them in a TreeSet that contains objects belonging
-    * to the wrapper class Integer.  The set must be enclosed
-    * between square brackets and must contain a list of zero or
-    * more non-negative integers, separated by commas.  Spaces
-    * are allowed anywhere.  If the input is not of the correct
-    * form, an IllegalArgumentException is thrown.
-    */
-   private static TreeSet<Integer> readSet() {
+    /**
+     * Reads the sets provided from the user's input and perform the calculation.
+     *
+     * @throws IllegalArgumentException if an operator is not provided.
+     */
+    private static void calculateSets() {
 
-      TreeSet<Integer> set = new TreeSet<Integer>();  // The set that will be read.
+        TreeSet<Integer> A;
+        TreeSet<Integer> B;
+        char operator;
 
-      TextIO.skipBlanks();
-      if (TextIO.peek() != '[')
-         throw new IllegalArgumentException("Expected '[' at start of set.");
-      TextIO.getAnyChar(); // Read the '['.
+        A = readNewSet(); // Read the first set.
 
-      TextIO.skipBlanks();
-      if (TextIO.peek() == ']') {
-            // The set has no integers.  This is the empty set, which
-            // is legal.  Return the value.
-         TextIO.getAnyChar(); // Read the ']'.
-         return set;
-      }
+        TextIO.skipBlanks();
 
-      while (true) {
-            // Read the next integer and add it to the set.
-         TextIO.skipBlanks();
-         if (! Character.isDigit(TextIO.peek()))
-            throw new IllegalArgumentException("Expected an integer.");
-         int n = TextIO.getInt(); // Read the integer.
-         set.add( new Integer(n) );
-         TextIO.skipBlanks();
-         if (TextIO.peek() == ']')
-            break;  // ']' marks the end of the set.
-         else if (TextIO.peek() == ',')
-            TextIO.getAnyChar(); // Read a comma and continue.
-         else
-            throw new IllegalArgumentException("Expected ',' or ']'.");
-      }
+        if (TextIO.peek() != '*'
+                && TextIO.peek() != '+'
+                && TextIO.peek() != '-') {
 
-      TextIO.getAnyChar(); // Read the ']' that ended the set.
+            throw new IllegalArgumentException("Expected *, +, or '-'");
+        }
 
-      return set;
+        operator = TextIO.getAnyChar(); // Read the operator.
 
-   } // end readSet()
-}
+        B = readNewSet(); // Read the second set.
+
+        TextIO.skipBlanks();
+
+        if (TextIO.peek() != '\n') {
+
+            throw new IllegalArgumentException("Extra unexpected input.");
+        }
+
+        // Perform the calculations with the given sets.
+
+        switch (operator) {
+            case '+' -> A.addAll(B);
+            case '-' -> A.removeAll(B);
+            case '*' -> A.retainAll(B);
+            default -> {
+            } // Do nothing, as no operator was given.
+        }
+        System.out.print("Value of A is  " + A);
+
+    } // end of calculateSets() method.
+
+
+    /**
+     * Reads a set of integers from the user's input,
+     * and stores them in a TreeSet to prevent non-negative numbers.
+     * Must contain a starting "[", followed by integers seperated by a comma, and end with a "]".
+     *
+     * @throws IllegalArgumentException If the input is not of the correct form.
+     */
+    private static TreeSet<Integer> readNewSet() {
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        TextIO.skipBlanks();
+
+        if (TextIO.peek() != '[') {
+            throw new IllegalArgumentException("Expected '[' at start of set.");
+        }
+        TextIO.getAnyChar(); // Read the '['.
+
+        TextIO.skipBlanks();
+        if (TextIO.peek() == ']') {
+
+            TextIO.getAnyChar(); // Read the ']'.
+            return set;
+        }
+
+        while (true) {
+
+            TextIO.skipBlanks();
+
+            if (!Character.isDigit(TextIO.peek())) {
+                throw new IllegalArgumentException("Expected an integer.");
+            }
+
+            int n = TextIO.getInt(); // Read the integer.
+            set.add(n);
+            TextIO.skipBlanks();
+
+            if (TextIO.peek() == ']') {
+                break;
+            } else if (TextIO.peek() == ',') {
+                TextIO.getAnyChar(); // Read a comma.
+            } else
+                throw new IllegalArgumentException("Expected ',' or ']'");
+        }
+        TextIO.getAnyChar(); // Read the final ']'.
+
+        return set;
+
+    } // end readNewSet()
+} //end of class
