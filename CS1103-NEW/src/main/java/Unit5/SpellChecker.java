9b756eb20fd0e4d2db1bb3091ee30d2c21ce9d19
Renaldo Swanepoel <renaldo@ibureau.services>
2023-05-10 08:11:30 +0200

    Allow for the file to read and added the contents to a Set

--- CS1103-NEW/src/main/java/Unit5/SpellChecker.java
+++ CS1103-NEW/src/main/java/Unit5/SpellChecker.java
@@ -4,6 +4,7 @@ import Test.TextIO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class SpellChecker {
@@ -12,9 +13,19 @@ public class SpellChecker {
 
         // Reading in the Dictionary
         try {
-            Scanner fileIn = new Scanner(new File("\\words.txt"));
+            // Please change the full path to where you are keeping the file.
+            File file = new File("D:\\Edu Projects\\UoPeople\\CS1103-NEW\\src\\main\\java\\Unit5\\words.txt");
+            Scanner fileIn = new Scanner(file);
+            HashSet<String> fileSet = new HashSet<>();
+
+            while (fileIn.hasNext()) {
+                String token = fileIn.next();
+                //do something with token
+                fileSet.add(token.toLowerCase());
+            }
+            System.out.println(fileSet.size());
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Couldn't find the file, please check the path.");
         }
     }
 }
