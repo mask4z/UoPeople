737cd4ca9a2724a02e877aa6fed25f78e05b4220
Renaldo Swanepoel <renaldo@ibureau.services>
2023-05-18 18:00:43 +0200

    Got recursion to work

--- CS1103-NEW/src/main/java/Unit6/textcollage/DirectoryList.java
+++ CS1103-NEW/src/main/java/Unit6/textcollage/DirectoryList.java
@@ -1,6 +1,9 @@
 package Unit6.textcollage;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -12,32 +15,46 @@ import java.util.Scanner;
 public class DirectoryList {
 
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      String directoryName;  // Directory name entered by the user.
-      File directory;        // File object referring to the directory.
-      String[] files;        // Array of file names in the directory.
-      Scanner scanner;       // For reading a line of input from the user.
+        String directoryName;  // Directory name entered by the user.
+        File directory;        // File object referring to the directory.
+        ArrayList<String> files = new ArrayList<>();        // Array of file names in the directory.
+        Scanner scanner;       // For reading a line of input from the user.
 
-      scanner = new Scanner(System.in);  // scanner reads from standard input.
+        scanner = new Scanner(System.in);  // scanner reads from standard input.
 
-      System.out.print("Enter a directory name: ");
-      directoryName = scanner.nextLine().trim();
-      directory = new File(directoryName);
+        System.out.print("Enter a directory name: ");
+        directoryName = scanner.nextLine().trim();
+        directory = new File(directoryName, "test");
 
-      if (directory.isDirectory() == false) {
-         if (directory.exists() == false)
-            System.out.println("There is no such directory!");
-         else
-            System.out.println("That file is not a directory.");
-      }
-      else {
-         files = directory.list();
-         System.out.println("Files in directory \"" + directory + "\":");
-         for (int i = 0; i < files.length; i++)
-            System.out.println("   " + files[i]);
-      }
+        if (!directory.isDirectory()) {
+            if (!directory.exists())
+                System.out.println("There is no such directory!");
+            else
+                System.out.println("That file is not a directory.");
+        } else {
+            files.add(String.valueOf(directory));
+            System.out.println("Files in directory \"" + directory + "\":");
+            getFiles(directory);
+        }
 
-   } // end main()
+    } // end main()
+
+    public static void getFiles(File file) {
+
+        String[] files = file.list();
+        for (String anotherFile : Objects.requireNonNull(file.list())) {
+            File newFile = new File(file, anotherFile);
+            if (newFile.isDirectory()) {
+                getFiles(newFile);
+            }
+            System.out.println("Files in directory \"" + file + "\":");
+            for (String s : files) {
+                System.out.println(s);
+            }
+        }
+
+    }
 
 } // end class DirectoryList
