b8bd4112ed4c7a5e4fe7a4d333e70fcb7fdd047a
Renaldo Swanepoel <renaldo.swanepoel16@gmail.com>
2023-05-22 21:17:43 +0200

    Discussion Assignment Post

--- CS1103-NEW/src/main/java/Test/Unit7DiscussionExample/MovieModel.java
+++ CS1103-NEW/src/main/java/Test/Unit7DiscussionExample/MovieModel.java
@@ -1,4 +1,26 @@
 package Test.Unit7DiscussionExample;
 
+import java.util.List;
+
 public class MovieModel {
+
+    private List<String> movies;
+
+    public List<String> getMovies() {
+
+        // movies = getAllMoviesFromDataBase();
+
+        return movies;
+    }
+
+    /*
+    public List<String> getAllMoviesFromDataBase() {
+
+        //String query = "SELECT mv FROM movies ORDER BY releasedate DESC"
+        // perform query
+
+        return listOfMovies;
+    }
+
+     */
 }
