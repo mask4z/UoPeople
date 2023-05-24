001736dac59dd170f971d68a3ae8f2911223b797
Renaldo Swanepoel <renaldo@ibureau.services>
2023-05-24 17:49:27 +0200

    Update to WebServer

--- CS1103-NEW/src/main/java/Unit7/ProgrammingAssignment/WebServer.java
+++ CS1103-NEW/src/main/java/Unit7/ProgrammingAssignment/WebServer.java
@@ -3,11 +3,15 @@ package Unit7.ProgrammingAssignment;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class WebServer {
 
     static final Integer LISTENING_PORT = 50505;
+    static final String rootDirectory = "C:\\Users\\Renaldo\\Desktop\\"; //Set up root directory
 
     public static void main(String[] args) {
         ServerSocket serverSocket;
@@ -34,45 +38,79 @@ public class WebServer {
 
     public static void handleConnection(Socket connection) throws IOException, RuntimeException {
 
-        try (connection; connection) {
+        PrintWriter pOut;
+
+        try {
 
             Scanner in = new Scanner(connection.getInputStream());
             OutputStream out = connection.getOutputStream();
-            PrintWriter pOut = new PrintWriter(out);
-            String rootDirectory = ""; //Set up root directory
+            pOut = new PrintWriter(out);
+            String line = in.nextLine();
+            List<String> words = Arrays.stream(line.split(" ")).toList();
 
-            while (true) {
-                if (!in.hasNext()) {
-                    break;
+            if (!words.get(0).contentEquals("GET")) {
+                sendErrorResponse(501, out);
+                connection.close();
+            }
+            if (in.tokens().count() != 3) {
+                sendErrorResponse(400, out);
+                connection.close();
+            }
+            System.out.println(line);
+
+            File currentWorkingDirectory = new File(rootDirectory);
+            String pathToFile = words.get(1).substring(1);
+            if (words.get(1).startsWith("/")) {
+
+                if (!currentWorkingDirectory.exists()) {
+                    sendErrorResponse(404, out);
+                    connection.close();
+                    return;
                 }
-                String request = in.nextLine();
-                if (request.trim().length() == 0) {
-                    break;
-                }
-                if (request.trim().startsWith("GET")) {
-                    System.out.println(request);
+                if (currentWorkingDirectory.listFiles() != null
+                        && Objects.requireNonNull(currentWorkingDirectory.listFiles()).length != 0) {
 
-                    if (request.trim().substring(4).startsWith("/")) {
-                        String pathToFile = request.trim().substring(3);
-                        File file = new File(rootDirectory + pathToFile);
-
-                        if (!file.exists()) {
-                            pOut.print("HTTP/1.1 404 File Not Found\r\n\r\n");
-                            pOut.flush();
-                            throw new RuntimeException("File not found");
+                    String response = getResponseHeaders(currentWorkingDirectory);
+                    response += "Content-type: text/plain\r\n";
+                    pOut.println(response);
+                    pOut.println("Directory: " + currentWorkingDirectory.getName());
+                    for (File file :
+                            currentWorkingDirectory.listFiles()) {
+                        if (file.isDirectory()) {
+                            pOut.println("  dir: " + file.getName());
                         }
-                        //Handle file
+                        pOut.println("  file: " + file.getName());
                     }
+                    pOut.flush();
+                    connection.close();
                 } else {
-                    pOut.print("HTTP/1.1 405 Method Not Allowed\r\n\r\n");
+                    pOut.println(getResponseHeaders(currentWorkingDirectory));
+                    pOut.println("Content-Type: text/plain\r\n");
+                    pOut.println("No such file/s on Webserver.");
                     pOut.flush();
-                    throw new RuntimeException("Method not allowed");
+                    connection.close();
                 }
-                if (in.tokens().count() != 3) {
-                    pOut.print("HTTP/1.1 Error: Invalid tokens\r\n\r\n");
-                    pOut.flush();
-                    throw new RuntimeException("Invalid tokens");
-                }
+            }
+            File selectedFile = new File(rootDirectory + pathToFile);
+
+            if (!selectedFile.exists()) {
+                sendErrorResponse(404, out);
+                connection.close();
+                return;
+            }
+            if (selectedFile.isDirectory()) {
+                sendErrorResponse(403, out);
+                connection.close();
+                return;
+            }
+            if (!selectedFile.canRead()) {
+                sendErrorResponse(403, out);
+                connection.close();
+                return;
+            } else {
+                pOut.println(getResponseHeaders(selectedFile));
+                pOut.flush();
+                sendFile(selectedFile, out);
             }
         } catch (IOException io) {
             throw new RuntimeException(io);
@@ -82,5 +120,43 @@ public class WebServer {
             connection.close();
             System.out.println("Connection closed");
         }
+    }
+
+    private static void sendFile(File selectedFile, OutputStream out) {
+
+    }
+
+    private static String getResponseHeaders(File currentWorkingDirectory) {
+        return null;
+    }
+
+    private static void sendErrorResponse(int responseCode, OutputStream out) {
+
+    }
+
+    private static String getMimeType(String fileName) {
+        int pos = fileName.lastIndexOf('.');
+        if (pos < 0)  // no file extension in name
+            return "x-application/x-unknown";
+        String ext = fileName.substring(pos + 1).toLowerCase();
+        if (ext.equals("txt")) return "text/plain";
+        else if (ext.equals("html")) return "text/html";
+        else if (ext.equals("htm")) return "text/html";
+        else if (ext.equals("css")) return "text/css";
+        else if (ext.equals("js")) return "text/javascript";
+        else if (ext.equals("java")) return "text/x-java";
+        else if (ext.equals("jpeg")) return "image/jpeg";
+        else if (ext.equals("jpg")) return "image/jpeg";
+        else if (ext.equals("png")) return "image/png";
+        else if (ext.equals("gif")) return "image/gif";
+        else if (ext.equals("ico")) return "image/x-icon";
+        else if (ext.equals("class")) return "application/java-vm";
+        else if (ext.equals("jar")) return "application/java-archive";
+        else if (ext.equals("zip")) return "application/zip";
+        else if (ext.equals("xml")) return "application/xml";
+        else if (ext.equals("xhtml")) return "application/xhtml+xml";
+        else return "x-application/x-unknown";
+        // Note:  x-application/x-unknown  is something made up;
+        // it will probably make the browser offer to save the file.
     }
 }
